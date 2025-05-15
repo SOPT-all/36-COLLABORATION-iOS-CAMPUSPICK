a884509d6745687faadea38518a05384457a6136
Yeonnies <eliyeon16@gmail.com>
2025-05-16 04:42:19 +0900

    [Feat] #6 - enum 추가

---
+++ 36-COLLABORATION-iOS-CAMPUSPICK/Presentation/Search/Model/Enum/DayType.swift
@@ -0,0 +1,35 @@
+//
+//  DayType.swift
+//  36-COLLABORATION-iOS-CAMPUSPICK
+//
+//  Created by 김나연 on 5/16/25.
+//
+
+enum DayType: CaseIterable {
+    case all
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+    case weekend
+    
+    func title() -> String {
+        switch self {
+        case .all:
+            return "전체"
+        case .monday:
+            return "월"
+        case .tuesday:
+            return "화"
+        case .wednesday:
+            return "수"
+        case .thursday:
+            return "목"
+        case .friday:
+            return "금"
+        case .weekend:
+            return "주말"
+        }
+    }
+}
