c0891e23ce9de7764c6084bf5fb754f403e45b07
KooWihC <ahnchiwook@hanmail.net>
2025-05-16 15:35:34 +0900

    [Feat] #8 - ViewContoller 추가

---
+++ 36-COLLABORATION-iOS-CAMPUSPICK/Presentation/Home/VIewController/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  36-COLLABORATION-iOS-CAMPUSPICK
+//
+//  Created by 안치욱 on 5/16/25.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class HomeViewController: UIViewController {
+    
+    
+}
