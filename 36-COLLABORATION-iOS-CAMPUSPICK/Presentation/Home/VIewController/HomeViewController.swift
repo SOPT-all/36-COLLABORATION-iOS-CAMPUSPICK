1b039d7e1b12c7ba040aabac2501a022f24558bb
KooWihC <ahnchiwook@hanmail.net>
2025-05-16 20:52:33 +0900

    [Feat] #8 - Scroll view add

--- 36-COLLABORATION-iOS-CAMPUSPICK/Presentation/Home/VIewController/HomeViewController.swift
+++ 36-COLLABORATION-iOS-CAMPUSPICK/Presentation/Home/VIewController/HomeViewController.swift
@@ -11,11 +11,37 @@ import Then
 
 final class HomeViewController: UIViewController {
     
+    private let headerView = MainHeaderView()
     
+    private let scrollView = UIScrollView().then {
+        $0.backgroundColor = .systemMint
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUI()
+        setLayout()
     }
     
+    private func setUI() {
+        view.addSubviews(
+            headerView,
+            scrollView
+        )
+    }
+    
+    private func setLayout() {
+        headerView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(-29)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(24)
+        }
+        scrollView.snp.makeConstraints {
+            $0.top.equalTo(headerView.snp.bottom).offset(14)
+            $0.horizontalEdges.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
     
 }
