b0e28274a24f4edb0364d87ae067101b27f420dd
OneTen19 <khy000426@naver.com>
2025-05-16 14:54:47 +0900

    [Feat] #11 - ClubRankingModel

---
+++ 36-COLLABORATION-iOS-CAMPUSPICK/Presentation/Club/Model/ClubRankingModel.swift
@@ -0,0 +1,23 @@
+//
+//  ClubRankingModel.swift
+//  36-COLLABORATION-iOS-CAMPUSPICK
+//
+//  Created by OneTen on 5/16/25.
+//
+
+struct ClubRankingModel {
+    let id: Int
+    let clubName: String
+    let clubIntroduce: String
+    let clubImage: String
+}
+
+extension ClubRankingModel {
+    static func dummy() -> [ClubRankingModel] {
+        return [
+            ClubRankingModel(id: 1, clubName: "더미1", clubIntroduce: "더미1 소개", clubImage: "person.circle"),
+            ClubRankingModel(id: 2, clubName: "더미2", clubIntroduce: "더미2 소개", clubImage: "person.circle"),
+            ClubRankingModel(id: 3, clubName: "더미3", clubIntroduce: "더미3 소개", clubImage: "person.circle")
+        ]
+    }
+}
