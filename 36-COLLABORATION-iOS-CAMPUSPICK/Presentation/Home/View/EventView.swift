9d9366b1a3bc0600531b0db3200b1ba711239496
KooWihC <ahnchiwook@hanmail.net>
2025-05-19 19:41:57 +0900

    [Feat] #8 - 이벤트 view 추가

---
+++ 36-COLLABORATION-iOS-CAMPUSPICK/Presentation/Home/View/EventView.swift
@@ -0,0 +1,88 @@
+//
+//  EventView.swift
+//  36-COLLABORATION-iOS-CAMPUSPICK
+//
+//  Created by 안치욱 on 5/19/25.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class EventView: UIView {
+    
+    
+    // MARK: - Properties
+    
+    private let titleLabel = UILabel().then {
+        $0.attributedText = .sopt("이벤트", style: .heading1)
+    }
+    
+    private lazy var buttonView = UIView().then {
+        $0.isUserInteractionEnabled = true
+        $0.addGestureRecognizer(self.touchViewGesture)
+    }
+    private let buttonLabel = UILabel().then {
+        $0.attributedText = .sopt("더 보기", style: .caption3)
+        $0.textColor = .gray2
+    }
+    private let moreButton = UIButton().then {
+        $0.setImage(.arrowSmall, for: .normal)
+        $0.isUserInteractionEnabled = false
+    }
+    private let touchViewGesture = UIGestureRecognizer(target: EventView.self, action: #selector(moreButtonTap))
+    
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setLayout()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - Setting Method
+    
+    private func setUI() {
+        self.addSubviews(
+            titleLabel,
+            buttonView
+        )
+        buttonView.addSubviews(
+            buttonLabel,
+            moreButton
+        )
+    }
+    
+    private func setLayout() {
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(4)
+            $0.leading.equalToSuperview().inset(15)
+        }
+        buttonView.snp.makeConstraints {
+            $0.centerY.equalTo(titleLabel.snp.centerY)
+            $0.trailing.equalToSuperview().inset(20)
+        }
+        /// 이거 뭐가 문제지..
+        buttonLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
+        moreButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview()
+        }
+    }
+    
+    
+    // MARK: - Target Method
+    
+    @objc private func moreButtonTap() {
+        print("더 보기 버튼 눌림!!")
+    }
+}
