0024b251b9c933ccb6999bb0bf75e81b1f611a50
Yeonnies <eliyeon16@gmail.com>
2025-05-21 03:04:31 +0900

    [Refactor] #19 - 검색후 UI 대폭 수정

--- 36-COLLABORATION-iOS-CAMPUSPICK/Presentation/Search/ViewController/SearchResultViewController.swift
+++ 36-COLLABORATION-iOS-CAMPUSPICK/Presentation/Search/ViewController/SearchResultViewController.swift
@@ -11,10 +11,27 @@ import Then
 
 class SearchResultViewController: UIViewController {
     
+    private let keyword: String
     private let searchData = SearchModel.dummy()
     
+    init(keyword: String) {
+        self.keyword = keyword
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     // MARK: - Property
+    
+    private let clubHeaderView = ClubHeaderView(type: .basic)
+    
+    private let separatorLine = UIView().then {
+        $0.backgroundColor = .gray4
+    }
+    
+    private let clubSortView = ClubSortView()
     
     private let searchTableView = UITableView(frame: .zero, style: .plain)
     
@@ -26,6 +43,12 @@ class SearchResultViewController: UIViewController {
         setStyle()
         setLayout()
         setDelegate()
+        addTarget()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
     }
     
     
@@ -33,13 +56,31 @@ class SearchResultViewController: UIViewController {
     
     private func setStyle() {
         view.backgroundColor = .white
-        view.addSubview(searchTableView)
-        searchTableView.contentInset.top = -15
+        view.addSubviews(clubHeaderView, separatorLine, clubSortView, searchTableView)
     }
     
     private func setLayout() {
+        clubHeaderView.snp.makeConstraints{
+            $0.top.equalToSuperview().inset(47)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(150)
+        }
+        
+        separatorLine.snp.makeConstraints{
+            $0.top.equalTo(clubHeaderView.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(7)
+        }
+        
+        clubSortView.snp.makeConstraints{
+            $0.top.equalTo(separatorLine.snp.bottom).offset(15)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(16)
+        }
+        
         searchTableView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
+            $0.top.equalTo(clubSortView.snp.bottom).offset(25)
+            $0.bottom.trailing.leading.equalToSuperview()
         }
     }
     
@@ -50,6 +91,35 @@ class SearchResultViewController: UIViewController {
             $0.separatorStyle = .none
             $0.register(SearchTableViewCell.self, forCellReuseIdentifier: SearchTableViewCell.identifier)
         }
+    }
+    
+    
+    // MARK: - Function
+    
+    private func addTarget() {
+        clubHeaderView.filterButton.addTarget(self, action: #selector(openFilterModal), for: .touchUpInside)
+        clubHeaderView.setSearchKeyword(keyword)
+        clubSortView.bottomSheetButton.addTarget(self, action: #selector(openSortModal), for: .touchUpInside)
+    }
+    
+    @objc private func openFilterModal() {
+        let modalVC = SearchFilterModalViewController()
+        if let sheet = modalVC.sheetPresentationController {
+            sheet.detents = [.custom(resolver: { _ in return 454 })]
+            sheet.prefersGrabberVisible = true
+            sheet.preferredCornerRadius = 15
+        }
+        present(modalVC, animated: true)
+    }
+    
+    @objc private func openSortModal() {
+        let modalVC = SearchSortModalViewController()
+        if let sheet = modalVC.sheetPresentationController {
+            sheet.detents = [.custom(resolver: { _ in return 246 })]
+            sheet.prefersGrabberVisible = true
+            sheet.preferredCornerRadius = 15
+        }
+        present(modalVC, animated: true)
     }
 }
 
